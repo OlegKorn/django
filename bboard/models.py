cc0eacaac210c585c335520ac3710bad12c2d916
OlegKorn <kornilovoy@gmail.com>
2019-08-13 12:40:58 +0300

    test

--- bboard/models.py
+++ bboard/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 
 class Bb(models.Model):
-    title = models.CharField(max_length = 50)
-    content = models.TextField(null=True, blank=True)
-    price = models.FloatField(null=True, blank=True)
+    title = models.CharField(max_length = 50, verbose_name = 'Title')
+    content = models.TextField(null=True, blank=True, verbose_name = 'Description')
+    price = models.FloatField(null=True, blank=True, verbose_name = 'Price')
     published = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        verbose_name_plural = 'Ads'
+        verbose_name = 'Ad'
+        ordering = ['-published']
 
     
  
