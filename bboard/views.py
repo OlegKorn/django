3fcba67cee4be90ce2935628cb8019471e81a0a5
OlegKorn <kornilovoy@gmail.com>
2019-08-13 05:31:31 +0300

    first commit

--- bboard/views.py
+++ bboard/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 from .models import Bb
 
-def index(request):
-    s = 'List of ads\r\n\r\n\r\n'
-   
-    for bboard in Bb.objects.order_by('-published'):    #   - in -published is for descending sort
-        s += bboard.title + '\r\n' + bboard.content + '\r\n\r\n'
 
-    return HttpResponse(s, content_type = 'text/plain; charset=utf-8')
+def index(request):
+    bbs = Bb.objects.all()
+    return render(
+        request, 
+        'bboard/index.html/', 
+        {'bbs' : bbs}
+    )
--- samplesite/urls.py
+++ samplesite/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from bboard.views import index
 
 
 urlpatterns = [
-    path('bboard/', include('bboard.urls')),
+    path('bboard/', index),
     path('admin/', admin.site.urls),
 ]
