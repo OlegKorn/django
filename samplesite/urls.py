7f517bbeb77b3b3512c70a145c5c5a56e7d7e098
OlegKorn <kornilovoy@gmail.com>
2019-08-14 03:55:42 +0300

    + path('bboard/<int:rubric_id>/', by_rubric), in samplesite.urls

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from bboard.views import index
+from bboard.views import index, by_rubric
 
 
 urlpatterns = [
     path('', index),
     path('bboard/', index),
+    path('bboard/<int:rubric_id>/', by_rubric),
     path('admin/', admin.site.urls)
 ]
